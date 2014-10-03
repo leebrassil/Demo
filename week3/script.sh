290b1505c85340d1a4291411ffe782bc69b5132b
leebrassil <leebrassil@hotmail.com>
2014-10-03 15:43:13 +0100

    added comments

--- week3/script.sh
+++ week3/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#: Title : cpucount.sh
+#: Title : script.sh
 #: Date : 3/10/14
 #: Author : Lee Brassil
 #: Version : 1.0
-#: Description : cpu count script
+#: Description : counts number of cpu's in a machine
 #: Options :
 cpucnt=$( grep 'processor' /proc/cpuinfo | wc -l )
 printf "Number of CPU's is: %s\n" $cpucnt
