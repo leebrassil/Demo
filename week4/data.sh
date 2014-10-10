78014d74684648720abd1ebe352ab8aaaab7f1c4
leebrassil <leebrassil@hotmail.com>
2014-10-10 14:41:12 +0100

    data.sh added

---
+++ week4/data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#: Title : data.sh
+#: Date : 10/10/14
+#: Author : Lee Brassil
+#: Version :	1.0
+#: Description : collects info from user in commandline and prints out info
+#: Options : 
+echo "Enter your name"
+read name
+echo "Enter the omnth you were born in"
+read month
+echo "Enter your favourite colour"
+read colour
+printf "Hello $name you were born in $month and your favourite colour is $colour" 
