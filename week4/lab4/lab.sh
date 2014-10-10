6786d6e2c21bcd58ef3eda229ac9de5bfd8ee139
leebrassil <leebrassil@hotmail.com>
2014-10-10 15:49:03 +0100

    lab.sh fully implemented

--- week4/lab4/lab.sh
+++ week4/lab4/lab.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-
+#: Title : lab.sh
+#: Date : 10/10/14
+#: Author : Lee Brassil
+#: Version :	1.0
+#: Description : lab 4 script
+#: Options : 
+echo "Input is $1"
+if [ "$1" == "cache" ] || [ "$1" == "speed" ]; then
+	echo "$1 is allowed"
+	info=$( grep "$1" /proc/cpuinfo )
+	if [ "$info" == "" ]; then
+		printf "No %s info found" "$1"
+	else
+		printf "Found:\n %s\n" "$info"
+	fi	
+else
+	printf "Input must be speed or cache\n"
+fi
