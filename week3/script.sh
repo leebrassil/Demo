abc22d9f8f5b046ecd09e8ad6028a7e837105831
leebrassil <leebrassil@hotmail.com>
2014-10-24 16:12:11 +0100

    cron scheduling done

--- week3/script.sh
+++ week3/script.sh
@@ -6,4 +6,4 @@
 #: Description : counts number of cpu's in a machine
 #: Options :
 cpucnt=$( grep 'processor' /proc/cpuinfo | wc -l )
-printf "Number of CPU's is: %s\n" $cpucnt
+printf "Number of CPU's is: %s\n" $cpucnt >> /home/mint/Demo/week3/log.txt
