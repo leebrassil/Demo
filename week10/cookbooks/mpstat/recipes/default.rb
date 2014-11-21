2dd719b5b089840a3b640df2026fd913d04b0c5b
leebrassil <leebrassil@hotmail.com>
2014-11-21 15:00:41 +0000

    default.rb modified

--- week10/cookbooks/mpstat/recipes/default.rb
+++ week10/cookbooks/mpstat/recipes/default.rb
@@ -6,3 +6,18 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+package "sysstat" do
+	action :install
+end
+
+user "sysmon" do
+	action :create
+	comment "user of sysstat"
+	home "/home/sysmon"
+	shell "/bin/bash"
+	supports :manage_home => true
+end
+
+file "/home/sysmon/config.dat" do
+	action :create
+end
